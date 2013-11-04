48ad1ea187c90c1b1cb99d6e8c2d655a6de4a0d9
wood7258 <wood7258@uw.edu>
2013-11-04 01:17:12 -0800

    required content

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,37 @@
 //signup.js 
 // add your JavaScript code to this file
 
+$(function() {
+	populateStates();
+	
+	$('.signup-form').submit(function(){
+		var signupForm = $(this);
+		var addr1Input = signupForm.find('input[name="addr-1"]');
+		var addr1Value = addr1Input.val();
+		if(addr1Value.length == 0) {
+			alert('INPUT REQUIRED: ADDRESS\nPLEASE ADVISE.');
+			return false;
+		}
+		var zipInput = signupForm.find('input[name="zip"]');
+		var zipValue = zipInput.val();
+		if(zipValue.length == 0) {
+			alert('INPUT REQUIRED: ZIP CODE\nPLEASE ADVISE.');
+			return false;
+		}
+	});
+	$('.cancel-signup').click(function(){
+		alert('Y U GOTTA B LIEK DIS?!?!?!');
+		window.location = 'http://www.google.com';
+	});
+});
+
+function populateStates() {
+	var stateSelect = $('.us-states');
+	var option;
+	$.each(usStates, function(i, state) {
+		option = $(document.createElement('option'));
+		option.attr('value', state.abbreviation);
+		option.html(state.name);
+		stateSelect.append(option);
+	});
+}
